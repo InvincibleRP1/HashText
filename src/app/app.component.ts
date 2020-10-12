02f69213cce361e0475badf4145143db5a4b7b83
Rahul <67526478+InvincibleRP2@users.noreply.github.com>
2020-10-12 15:48:07 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HashText';
+  encryptMode: boolean;
+  textToConvert: string;
+  password: string;
+  conversionOutput: string;
+
+  constructor() {
+    this.encryptMode = true;
+  }
+
+  changeMode() {
+    this.textToConvert = "";
+    this.password = "";
+
+  }
+
+  convertText() {
+    if (this.textToConvert.trim() === "" || this.password.trim() === "") {
+      this.conversionOutput = "BOTH TEXTBOXES MUST BE FILLED!"
+      return;
+    }
+    else {
+      if (this.encryptMode) {
+        this.conversionOutput = CryptoJS.AES.encrypt(this.textToConvert.trim(), this.password.trim()).toString();
+      }
+      // else {
+      //   this.conversionOutput = CryptoJS.AES.decrypt(this.textToConvert.trim(), this.password.trim()).toString(CryptoJS.enc.Utf8);
+      // }
+    }
+  }
 }
+
+
